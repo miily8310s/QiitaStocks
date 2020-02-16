d48c377662c8ebcfe185769fd2e03138816e95ac
miily8310s <rillakerika@gmail.com>
2020-02-17 07:32:39 +0900

    Add Link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <div className="container">
                 <a className="navbar-brand" href="/">Qiita Stocks</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Moment from 'moment';
 
 const Note = (props) => {
     const { note } = props
     const tags = note.tags
+    const url = note.url
     const styles = {
         ul:{
-            maxWidth:700, 
+            maxWidth:500, 
             whiteSpace: 'nowrap', 
             overflow: 'hidden',
             textOverflow: 'ellipsis'
           },
         tagul:{
+            margin: '0 5px 0 0',
+            padding: '0px 5px',
             background:'rgba(0,0,0,.03)',
-            borderColor: '#e9e9eb',
+            border: '1px solid #e9e9eb',
+            borderRadius: '3px',
             color: '#909399'
           }
     }
@@ -20,12 +25,12 @@ const Note = (props) => {
         <div className="col-md-6">
             <div className="card mb-4">
                 <div className="card-body">
-                    <h5>{note.title}</h5>
-                    <p className="card-subtitle">{note.created_at}</p>
+                    <a href={url}>{note.title}</a>
+                    <p className="card-subtitle">{Moment.utc(note.created_at).format('YYYY-MM-DD HH:mm')}</p>
                     <p className="card-subtitle">
                         {note.user.id}
                     </p>
-                    <p style={styles.ul} className="card-text mr-4">{note.body}</p>
+                    <p style={styles.ul} className="mr-4">{note.body}</p>
                     {tags.map(item => (
                         <span style={styles.tagul}>{item.name}</span>
                     ))}
